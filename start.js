b90de37aaca12908dbd6742431edc0891c2ed92c
hightouch67 <cetinkaya.ozden@gmail.com>
2024-10-16 12:12:44 +0200

    first commit

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { createPublishTx, getCreatePublishTx, sendTx } = require('./utils');
+const { createPublishTx, sendTx } = require('./utils');
 
 process.on('uncaughtException', err => {
     console.error('Uncaught exception', err);
@@ -9,7 +9,6 @@ let lastHour = 0;
 let isProcessing = false;
 const account = process.env.ACCOUNT
 const multisigAccounts = process.env.MULTISIG_ACCOUNTS.split(' ')
-const isFirstAccount = multisigAccounts.indexOf(account) === 0
 const isLastAccount = multisigAccounts.indexOf(account) === multisigAccounts.length - 1
 
 const check = setInterval(() => {
@@ -17,32 +16,25 @@ const check = setInterval(() => {
         const currentHour = new Date().getUTCHours();
         const currentMinutes = new Date().getMinutes();
         console.log('...', currentHour, currentMinutes);
-        if (currentHour !== lastHour && currentMinutes > multisigAccounts.indexOf(account) + 1) {
+        if (currentHour !== lastHour) {
             isProcessing = true;
             console.log('1/2 Start process', currentHour);
-            if (isFirstAccount) {
+            if (isLastAccount && currentMinutes > 4) {
+                sendTx(multisigAccounts[multisigAccounts.indexOf(account) - 1]).then((tx) => {
+                    lastHour = currentHour;
+                    isProcessing = false;
+                    console.log('Tx ', tx);
+                    console.log('2/2 Last tx sent at', lastHour);
+                })
+            }
+            else if(!isLastAccount)
                 createPublishTx().then((tx) => {
                     lastHour = currentHour;
                     lastTx = tx;
                     isProcessing = false;
+                    console.log('Tx ', tx);
                     console.log('2/2 Last signature sent at', lastHour);
                 })
-            }
-            else if (isLastAccount) {
-                sendTx(multisigAccounts[multisigAccounts.indexOf(account) - 1]).then(() => {
-                    lastHour = currentHour;
-                    isProcessing = false;
-                    console.log('2/2 Last tx sent at', lastHour);
-                })
-            }
-            else
-                getCreatePublishTx(multisigAccounts[multisigAccounts.indexOf(account) - 1]).then(() => {
-                    lastHour = currentHour;
-                    isProcessing = false;
-                    console.log('2/2 Last signature sent at', lastHour);
-                }).catch(err => {
-                    console.error('Set last Signing failed, action required', err);
-                });
         }
     }
 }, 1000 * 5);
--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
-const steem = require('steem');
 require('dotenv').config();
+const steem = require('steem');
+const multisigAccounts = process.env.MULTISIG_ACCOUNTS.split(' ')
+const username = process.env.ACCOUNT;
 
+let cachedBlockProps = {};
+let lastFetchedHour = -1;
 
 function getDynamicGlobalProperties() {
     return new Promise((resolve, reject) => {
@@ -14,10 +18,9 @@ function getDynamicGlobalProperties() {
     });
 }
 
-
-function sendTransaction(transaction, privateKey) {
+async function fetchGlobalProperties() {
     return new Promise((resolve, reject) => {
-        steem.broadcast.send(transaction, privateKey, (err, result) => {
+        steem.api.getDynamicGlobalProperties((err, result) => {
             if (err) {
                 reject(err);
             } else {
@@ -25,74 +28,100 @@ function sendTransaction(transaction, privateKey) {
             }
         });
     });
+}
+
+async function getRefBlockProps() {
+    const currentHour = new Date().getUTCHours();
+
+    // Check if we need to fetch new block properties
+    if (currentHour !== lastFetchedHour) {
+        try {
+            const globalProps = await fetchGlobalProperties();
+            const blockProps = {
+                ref_block_num: globalProps.head_block_number & 0xFFFF,
+                ref_block_prefix: Buffer.from(globalProps.head_block_id, 'hex').readUInt32LE(4),
+            };
+            cachedBlockProps[currentHour] = blockProps; // Cache the properties by hour
+            lastFetchedHour = currentHour;
+        } catch (error) {
+            console.error('Error fetching global properties:', error);
+            throw error; // Rethrow to indicate failure
+        }
+    }
+
+    return cachedBlockProps[currentHour];
+}
+
+
+function sendTransaction(transaction, privateKey) {
+    return new Promise((resolve, reject) => {
+        steem.broadcast.send(transaction, privateKey, (err, result) => {
+            if (err)
+                reject(err);
+            resolve(result);
+        });
+    });
+}
+
+function signTransaction(transaction) {
+    return  steem.auth.signTransaction(transaction, [process.env.ACTIVE_KEY])
 }
 
 function broadcastTransaction(signedTransaction) {
     return new Promise((resolve, reject) => {
         steem.api.broadcastTransaction(signedTransaction, (err, result) => {
-            if (err) {
+            if (err)
                 reject(err);
-            } else {
-                resolve(result);
-            }
+            resolve(result);
         });
     });
 }
 
-function getAccount(account) {
+function getAccounts(accounts) {
     return new Promise((resolve, reject) => {
-        steem.api.getAccounts([account], (err, result) => {
-            if (err) {
+        steem.api.getAccounts(accounts, (err, result) => {
+            if (err)
                 reject(err);
-            } else {
-                resolve(result);
-            }
+            resolve(result);
         });
     });
 }
 
 function createPublishTx() {
     return new Promise(async (resolve, reject) => {
-
-        const username = process.env.account
-        const expireTime = 1000 * 3590;
-        const globalProps = await getDynamicGlobalProperties();
-        const ref_block_num = globalProps.head_block_number & 0xFFFF;
-        const ref_block_prefix = Buffer.from(globalProps.head_block_id, 'hex').readUInt32LE(4);
+        const { ref_block_num, ref_block_prefix } = await getRefBlockProps();
 
         const transactionData = {
             operations: [
                 ['transfer',
                     {
-                        'amount': process.env.AMOUNT_SBD + ' SBD',
-                        'from': process.env.MULTISIG_ACCOUNT,
-                        'memo': '',
-                        'to': process.env.SEND_TO
+                        amount: `${process.env.AMOUNT_SBD} SBD`,
+                        from: process.env.TRANSFER_FROM,
+                        memo: process.env.MEMO,
+                        to: process.env.SEND_TO
                     }
                 ]
             ]
         };
-
+        const nextHourUTC = new Date();
+        nextHourUTC.setUTCMinutes(0, 0, 0); 
+        nextHourUTC.setUTCHours(nextHourUTC.getUTCHours() + 1);
         let transaction = {
             ref_block_num,
             ref_block_prefix,
-            expiration: new Date(Date.now() + expireTime).toISOString().slice(0, -5),
+            expiration: nextHourUTC.toISOString().slice(0, -5),
             operations: transactionData.operations,
             extensions: []
         };
-
-
-        const signedTransaction = steem.auth.signTransaction(transaction, [process.env.ACTIVE_KEY]);
-
-        const [account] = await getAccount(username)
+        const signedTransaction = signTransaction(transaction);
+        const [account] = await getAccounts([username])
         let json_metadata;
         try {
             json_metadata = JSON.parse(account.posting_json_metadata)
         } catch (error) {
             console.log(error)
         }
-        json_metadata.mtx = JSON.stringify(signedTransaction)
-
+        json_metadata.mtx = signedTransaction.signatures[0]
         let ops = [];
         ops.push(
             [
@@ -103,66 +132,83 @@ function createPublishTx() {
                     posting_json_metadata: JSON.stringify(json_metadata)
                 },
             ])
-        let finalTx = { operations: ops, extensions: [] };
-        const tx = await sendTransaction(finalTx, { posting: process.env.POSTING_KEY })
-        resolve(tx)
-    })
-}
-
-async function getCreatePublishTx(from) {
-    return new Promise(async (resolve, reject) => {
-
-
-        const username = process.env.account
-        const [fromAccount] = await getAccount(from)
-        const [account] = await getAccount(username)
-        let from_json_metadata = {};
-        let json_metadata = {};
         try {
-            from_json_metadata = JSON.parse(fromAccount.posting_json_metadata)
-            json_metadata = JSON.parse(account.posting_json_metadata)
-
+            const finalTx = { operations: ops, extensions: [] };
+            const tx = await sendTransaction(finalTx, { posting: process.env.POSTING_KEY })
+            resolve(tx)
         } catch (error) {
-            console.log(error)
+            reject(error)
         }
-        let previousTx = JSON.parse(from_json_metadata.mtx)
-
-        const signedTransaction = steem.auth.signTransaction(previousTx, [process.env.ACTIVE_KEY]);
-        json_metadata.mtx = JSON.stringify(signedTransaction)
-        let ops = [];
-        ops.push(
-            [
-                'account_update2',
-                {
-                    account: username,
-                    json_metadata: "",
-                    posting_json_metadata: JSON.stringify(json_metadata)
-                },
-            ])
-        let finalTx = { operations: ops, extensions: [] };
-        const tx = await sendTransaction(finalTx, { posting: process.env.POSTING_KEY })
-        resolve(tx)
     })
 }
 
-async function sendTx(from) {
+async function sendTx() {
     return new Promise(async (resolve, reject) => {
-        const [fromAccount] = await getAccount(from)
-        let from_json_metadata = {};
+        const accountsToFetch = multisigAccounts.filter(account => account !== username);
+        const fromAccounts = await getAccounts(accountsToFetch);
+        const signatures = [];
+
         try {
-            from_json_metadata = JSON.parse(fromAccount.posting_json_metadata)
+            // Collect signatures from accounts
+            for (const account of fromAccounts) {
+                if (account.posting_json_metadata) {
+                    const json_metadata = JSON.parse(account.posting_json_metadata);
+                    if (json_metadata.mtx) {
+                        signatures.push(json_metadata.mtx);
+                    } else {
+                        throw new Error('Missing mtx in json_metadata for account: ' + account.name);
+                    }
+                }
+            }
 
+            console.log('Collected signatures:', signatures);
+
+            // Get dynamic global properties
+            const [ref_block_num, ref_block_prefix] = await getDynamicGlobalProperties();
+
+            // Prepare transaction data
+            const transactionData = {
+                operations: [
+                    ['transfer', {
+                        amount: `${process.env.AMOUNT_SBD} SBD`,
+                        from: process.env.TRANSFER_FROM,
+                        memo: process.env.MEMO,
+                        to: process.env.SEND_TO
+                    }]
+                ]
+            };
+
+            // Calculate expiration as the next hour in UTC
+            const nextHourUTC = new Date();
+            nextHourUTC.setUTCMinutes(0, 0, 0);
+            nextHourUTC.setUTCHours(nextHourUTC.getUTCHours() + 1);
+
+            // Create the transaction object
+            let transaction = {
+                ref_block_num,
+                ref_block_prefix,
+                expiration: nextHourUTC.toISOString().slice(0, -5), // No 'Z' for expiration
+                operations: transactionData.operations,
+                extensions: [],
+                signatures
+            };
+
+            // Sign the transaction
+            const signedTransaction = signTransaction(transaction);
+
+            // Add collected signatures to the transaction
+            transaction.signatures = signedTransaction.signatures;
+
+            console.log('Signed transaction:', transaction);
+            
+            // Broadcast the transaction
+            const tx = await sendTransaction(transaction);
+            resolve(tx);
         } catch (error) {
-            console.log(error)
+            console.error('Error while processing transaction:', error);
+            reject(error);
         }
-        let previousTx = JSON.parse(from_json_metadata.mtx)
-
-
-        const signedTransaction = steem.auth.signTransaction(previousTx, [process.env.ACTIVE_KEY]);
-
-        const tx = await broadcastTransaction(signedTransaction)
-        resolve(tx)
-    })
+    });
 }
 
-module.exports = { createPublishTx, getCreatePublishTx, sendTx }
+module.exports = { createPublishTx, sendTx }
